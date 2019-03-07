b77237086784813c3e0e70dc40b604aa492cf175
Wessel Loth <w.loth@arcady.nl>
2019-03-07 20:11:49 +0100

    Rename to PokemonController

--- src/Pokedex/Controllers/PokemonController.cs
+++ src/Pokedex/Controllers/PokemonController.cs
@@ -8,35 +8,35 @@ namespace Pokedex.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class PokemonController : ControllerBase
     {
-        // GET api/values
+        // GET api/pokemon
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/pokemon/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/pokemon
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/pokemon/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/pokemon/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
