192b2bf305cc0c5c14a8eea698263eabae134a91
Wessel Loth <w.loth@arcady.nl>
2019-03-07 20:24:52 +0100

    Add Swagger & Debug Config

--- src/Pokedex/Startup.cs
+++ src/Pokedex/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Pokedex
 {
@@ -26,6 +27,12 @@ namespace Pokedex
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Pokémon API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +48,13 @@ namespace Pokedex
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // Configure Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pokémon API V1");
+            });
+
             app.UseMvc();
         }
     }
